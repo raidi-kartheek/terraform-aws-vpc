03b25bf1d64aa2d576bdb4e0588de02a14e8db9e
Your Name <you@example.com>
2026-06-29 13:20:28 +0530

    msg

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 #intercollaction
 locals {
-  resource_name = "${var.project_name}-${var.enivronment}" #inter  collision
+  resource_name = "${var.project_name}-${var.environment}" #inter  collision
   az_names = slice(data.aws_availability_zones.available.names, 0, 2)
 }
 # "${var.project_name}-${var.environment}"
